0efda8e38fc99f171b5e7f94b082088e309cbaa2
khalil smairi <smairio98@gmail.com>
2021-01-20 18:22:40 -0800

    1

--- TD3 Fonctions et procédures/exercice2.c
+++ TD3 Fonctions et procédures/exercice2.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 void sommeproduit(int n, int *som,int *prod){
     int p;
-    int sm=0;
-    int pr=1;
+    *som=0;
+    *prod=1;
     while (n/10 != 0){
         p=n%10;
         if(p%2==1){
-            pr=pr*p;
+            *prod*=p;
         }else{
-            sm=sm+p;
+            *som+=p;
         }
-        n/10;
+        n=n/10;
     }
-    *som=sm;
-    *prod=pr;
+
 }
 int main(){
     int x,somme,produit;
