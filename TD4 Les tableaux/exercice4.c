e605e8ad40f8bba4fe67c1285b49d93f7a9ef417
khalil smairi <smairio98@gmail.com>
2021-02-01 14:40:17 -0800

    s

--- TD4 Les tableaux/exercice4.c
+++ TD4 Les tableaux/exercice4.c
@@ -1,103 +1,84 @@
 #include <stdio.h>
-
-
-
-void taille(int *n);
-void remplir(int *t,int n);
-void affichage(int *t,int n);
-int b_inf(int *b_Inf);
-int b_sup(int *b_Sup);
-void getMinMax(int *t,int *min, int *max,int b_inf,int b_sup);
-
-
+void getMinMax(int *t,int borneInf,int borneSup,int *min,int *max);
+void xx(int *a,int *b);
+void permutation(int *t,int borneInf,int borneSup,int *min,int *max);
 int main(){
-    int n;
-    int t[200];
-    int min,max,b_Inf,b_Sup;
-    taille(&n);
-    remplir(t,n);
-    affichage(t,n);
-    b_inf(&b_Inf);
-    b_sup(&b_Sup);
-    getMinMax(t,&min,&max,b_Inf,b_Sup);
-    affichage(t,n);
-    printf("\n %d %d",min,max);
-    
-}
-
-
-
-void taille(int *n){
+    int t[200],i,n,borneInf,borneSup,min,max;
     do{
         printf("donner le taille du tableau:\n");
-        scanf("%d",n);
-    }while(*n<=0 || 200<*n);
-}
-
-
-
-void remplir(int *t,int n){
-    int i;
-    printf("remplir le tableau:\n");
+        scanf("%d",&n);
+    }while(n<0 || 200<n);
+    printf("remplir le tableau\n");
     for(i=0;i<n;i++){
-        scanf("%d",t+i);
+        scanf("%d",&t[i]);
     }
-}
 
-
-
-void affichage(int *t,int n){
+    
     printf("affichage du tableau:\n");
-    int i;
     for(i=0;i<n;i++){
-        printf("%d ",t[i]);
+        printf("%d  ",t[i]);
     }
-}
+    putchar('\n');
 
+    //borne inf
+    printf("donner la borne inferieur:\n");
+    do{scanf("%d",&borneInf);}while(borneInf<1 || n-2<borneInf);
+    borneInf--;
 
-
-int b_inf(int *b_inf){
-    printf("\ndonner la borne inferieur:\n");
-    scanf("%d",b_inf);
-    return *b_inf;
-}
-
-
-
-int b_sup(int *b_sup){
+    //borne supp
     printf("donner la borne supperieur:\n");
-    scanf("%d",b_sup);
-    return *b_sup;
+    do{scanf("%d",&borneSup);}while(borneSup<=borneInf || n-1<borneSup);
+    borneSup--;
 
+    ///////////////////////////////////////
+    permutation(t,borneInf,borneSup,&min,&max);
+    ///////////////////////////////////////
+
+
+    printf("affichage du tableau:\n");
+    for(i=0;i<n;i++){
+        printf("%d  ",t[i]);
+    }
+    /*
+    //affichage zone des borne
+     printf("affichage du zone:\n");
+    for(i=borneInf-1;i<=borneSup-1;i++){
+    printf("%d  ",t[i]);
+    }
+    putchar('\n');
+    getMinMax(t,borneInf,borneSup,&min,&max);
+    printf("min=%d && max=%d",min,max);
+    return 0;
+    */
 }
 
-
-
-void getMinMax(int *t,int *min, int *max,int b_inf,int b_sup){
+void getMinMax(int *t,int borneInf,int borneSup,int *min,int *max){
     int i;
-    *min=t[b_inf];
-    *max=t[b_inf];
-    for(i=b_inf;i<=b_sup;i++){
-        if(t[i]<*min){
+    *min=borneInf;
+    *max=borneSup;
+    for(i=borneInf;i<borneSup;i++){
+        if(t[i]<t[*min]){
             *min=i;
         }
-        if(t[i]>*max){
+        if(t[i]>t[*max]){
             *max=i;
         }
 
     }
 }
+void permutation(int *t,int borneInf,int borneSup,int *min,int *max){
+    int aux;
+    while((borneInf-borneSup!=0)){
+    getMinMax(t,borneInf,borneSup,min,max);
+    xx(&t[*min],&t[borneInf]);
+    borneInf++;
+    xx(&t[*max],&t[borneSup]);
+    borneSup--;
+    }
+}
 
-
-
-void permutation(int *a,int *b){
+void xx(int *a,int *b){
     *a=*a+*b;
     *b=*a-*b;
     *a=*a-*b;
-}
-
-
-
-void triMinMaxs(int *t){
-
 }
